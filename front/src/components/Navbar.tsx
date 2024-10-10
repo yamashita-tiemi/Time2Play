074d7c4b87b49d93ab23a70ef44ecb78ec8cd69d
Victoria <victoria.yamashita@estudante.ufjf.br>
2024-10-10 16:11:57 -0300

    iniciando styled component

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,19 +1,55 @@
-import * as React from "react";
+import styled from 'styled-components';
+import { NavLink } from "react-router-dom";
 
-function MyButton({ title }: { title: string }) {
-    return (
-        <button>{title}</button>
-    );
-}
+
+export const NavBar = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  backgroud-color: "0000";
+  width: 100%;
+  heigth: 80px
+`;
+
+export const MainNavbar = styled.ul`
+  list-style-type: none;
+  display: flex !important;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 40%;
+  margin-right: 2%;
+`;
+
+export const NavLi = styled.li`
+  text-align: center;
+  margin: 15px auto;
+` 
+
+export const LogoNavbar = styled.div`
+    width: 60%;
+    margin: auto 0 auto 2%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
   
 export default function Navbar() {
     return (
-        <div>
-            <h1>Welcome to my app</h1>
-            <MyButton title="I'm a button" />
-            <ul>
-                
-            </ul>
-        </div>
+        <NavBar>
+            <LogoNavbar>
+                <NavLink to={"/"}>Logo</NavLink>
+            </LogoNavbar>
+            <MainNavbar>
+                <NavLi>
+                    <NavLink to={"/"}>Page 1</NavLink>
+                </NavLi>
+                <NavLi>
+                    <NavLink to={"/"}>Page 2</NavLink>
+                </NavLi>
+                <NavLi>
+                    <NavLink to={"/"}>Login</NavLink>
+                </NavLi>
+            </MainNavbar>
+        </NavBar>
     );
 }
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import Navbar from "../components/Navbar"
+
 export const Home = () => {
     return(
-        <div>TESTE</div>
+        <div>
+            <Navbar/>
+            TESTE
+
+        </div>
     )
 }
